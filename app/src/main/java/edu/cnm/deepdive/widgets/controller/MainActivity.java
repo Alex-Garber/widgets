919ade895e7d32ad9cffffb09ea09ae516c4125b
Alex Garber <alexgarber6@gmail.com>
2020-11-12 15:14:10 -0700

    working on button

--- app/src/main/java/edu/cnm/deepdive/widgets/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/widgets/controller/MainActivity.java
@@ -11,8 +11,7 @@ import edu.cnm.deepdive.widgets.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-private ActivityMainBinding binding;
-
+    private ActivityMainBinding binding;
 
 
     @Override
@@ -20,41 +19,57 @@ private ActivityMainBinding binding;
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.covertToC.setOnClickListener((v) -> {
+            double value1 = Double.parseDouble(binding.inputf.getText().toString());
+            double value2 = Double.parseDouble(binding.inputc.getText().toString());
+            double value3 = Double.parseDouble(binding.inputk.getText().toString());
+           double cel = value1 - 32 / 1.8 ;
+            binding.inputf.setText(String.format("%.2f", cel));
+        });
 
-
-    }
-
-
-    private void computeAndDisplaySum() {
-        double Fahrenheit;
-        String input1 = binding.input1.getText().toString().trim();
-        if (input1.isEmpty()){
-            input1 = "0";
-            binding.input1.setText(input1);
-            Fahrenheit = 0;
-        }else {
-            Fahrenheit = Double.parseDouble(input1);
-        }
-        double Celsius;
-        String input2 = binding.input2.getText().toString().trim();
-        if (input2.isEmpty()){
-            input2 = "0";
-            binding.input2.setText(input1);
-            Celsius = 0;
-        }else {
-            Celsius = Double.parseDouble(input2);
-        }
-
-
-        double valueK;
-        String Kelvin = binding.input3.getText().toString().trim();
-        if (Kelvin.isEmpty()){
-            Kelvin = "0";
-            binding.input3.setText(input1);
-            valueK = 0;
-        }else {
-            valueK = Double.parseDouble(input1);
-        }
 
     }
 }
+
+
+//    private void computeAndDisplaySum() {
+//        double Fahrenheit;
+//        String input1 = binding.input1.getText().toString().trim();
+//        if (input1.isEmpty()){
+//            input1 = "0";
+//            binding.input1.setText(input1);
+//            Fahrenheit = 0;
+//        }else {
+//            Fahrenheit = Double.parseDouble(input1);
+//        }
+//        double Celsius;
+//        String input2 = binding.input2.getText().toString().trim();
+//        if (input2.isEmpty()){
+//            input2 = "0";
+//            binding.input2.setText(input1);
+//            Celsius = 0;
+//        }else {
+//            Celsius = Double.parseDouble(input2);
+//        }
+//
+//
+//        double valueK;
+//        String Kelvin = binding.input3.getText().toString().trim();
+//        if (Kelvin.isEmpty()){
+//            Kelvin = "0";
+//            binding.input3.setText(input1);
+//            valueK = 0;
+//        }else {
+//            valueK = Double.parseDouble(input1);
+//        }
+//
+//        double sumF = Fahrenheit;
+//        binding.output1.setText(getString(R.string.sum_resource, sumF));
+//
+//        double sumC = Fahrenheit - 32 / 1.8 ;
+//        binding.output2.setText(getString(R.string.sum_resource, sumC));
+//
+//        double sumK = (Fahrenheit + 459.67) * 5/9  ;
+//        binding.output3.setText(getString(R.string.sum_resource, sumK));
+//    }
+//}
